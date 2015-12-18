d0506ea54b81cb60ca91bc8fe74889fd17234640
Asia R <asiaracz@wp.pl>
2015-12-18 22:30:10 +0100

    ITcpLayer - started implementing Initialize() method

--- Pw.Elka.TIN/Server/Header Files/Components/TcpLayer/TcpLayer.h
+++ Pw.Elka.TIN/Server/Header Files/Components/TcpLayer/TcpLayer.h
@@ -2,11 +2,44 @@
 #include "../../Interfaces/ILayer.h"
 #include "../../Interfaces/ITcpLayerManager.h"
 
+#include <windows.h>
+#include <sys/types.h>
+#include <winsock.h>
+
 class TcpLayer :
 	public ILayer, public ITcpLayerManager
 {
 public:
 	TcpLayer();
 	~TcpLayer();
+//ITcpLayerManager intereface
+	bool Initialize(int socketfd, ILayer &upperLayer);
+	int End();
+//ILayer interface
+	void Send(char* buffer, int size) = 0;
+	void Receive(char* buffer, int size) = 0;
+
+private:
+//Socket used to communicate with client - descriptor returnet by socket()
+	int initCommunicationSocket;
+//Socket used for getting notification from Root Manager (information that client session ended) - descriptor returned by socket()
+	int initNotificationSocket;
+//Communication socket - descriptor returned by accept()
+	int communicationSocket;
+//Notification socket - descriptor returned by accept()
+	int notificationSocket;
+//Communication port number
+	int communicationPort;
+//Notification port number
+	int notificationPort;
+//Communication address structure
+	struct sockaddr_in communicationAddress;
+//Notification address structure
+	struct sockaddr_in notificationAddress;
+//Set of socket descriptors
+	fd_set socketsTcpLayer;
+
+
+
 };
 
--- Pw.Elka.TIN/Server/Source Files/Components/TcpLayer/TcpLayer.cpp
+++ Pw.Elka.TIN/Server/Source Files/Components/TcpLayer/TcpLayer.cpp
@@ -1,5 +1,75 @@
+//@todo - klasy wyj¹tków
 #include "../../../Header Files/Components/TcpLayer/TcpLayer.h"
 
+bool TcpLayer :: Initialize(int socketfd, ILayer &upperLayer)
+{
+	//Creating notification socket
+	if (initNotificationSocket = socket(AF_INET, SOCK_STREAM, 0) == -1)
+	{
+		throw "Socket() error - notificationSocket";
+	}
+
+	notificationAddress.sin_family = AF_INET;
+	notificationAddress.sin_addr.s_addr = htonl(INADDR_ANY);
+	notificationAddress.sin_port = htons(notificationPort);
+
+	if (bind(initNotificationSocket, (sockaddr*)&notificationAddress, sizeof notificationAddress) == -1)
+	{
+		throw "Bind() error - notificationSocket";
+	}
+
+	//@todo - check backlog number
+	listen(initNotificationSocket, 5);
+
+	if (notificationSocket = accept(initNotificationSocket, (sockaddr*)0, (int*)0) == -1)
+	{
+		throw "Accept() error - notificationSocket";
+	}
+
+	//Creating communication socket
+	if (initCommunicationSocket = socket(AF_INET, SOCK_STREAM, 0) == -1)
+	{
+		throw "Socket() error - communicationSocket"; 
+	}
+
+	communicationAddress.sin_family = AF_INET;
+	communicationAddress.sin_addr.s_addr = htonl(INADDR_ANY);
+	communicationAddress.sin_port = htons(communicationPort);
+
+	if (bind( initCommunicationSocket,(sockaddr*) &communicationAddress, sizeof communicationAddress)  == -1)
+	{
+		throw "Bind() error - communicationSocket";
+	}
+
+	//@todo - check backlog number
+	listen(initCommunicationSocket, 5);
+
+	if(communicationSocket = accept(initCommunicationSocket, (sockaddr*)0, (int*)0) == -1)
+	{
+		throw "Accept() error - communicationSocket";
+	}
+
+
+	//@todo 
+	return true;
+}
+
+int TcpLayer :: End()
+{
+	//@todo
+
+	return 0;
+}
+
+void TcpLayer :: Send(char* buffer, int size)
+{
+
+}
+
+void TcpLayer :: Receive(char* buffer, int size)
+{
+
+}
 
 
 TcpLayer::TcpLayer()
