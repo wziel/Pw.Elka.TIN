7c176bbbb901f5a1021a686187158af22796b457
Asia R <asiaracz@wp.pl>
2015-12-27 14:42:49 +0100

    ClientSession - start implementing IClientSessionManager - include <string>

--- Pw.Elka.TIN/Server/Header Files/Components/ClientSession/ClientSession.h
+++ Pw.Elka.TIN/Server/Header Files/Components/ClientSession/ClientSession.h
@@ -1,11 +1,31 @@
 #pragma once
 #include "../../Interfaces/IClientSessionManager.h"
 
-class ClientSession : 
+using namespace std;
+
+class ClientSession :
 	public IClientSessionManager
 {
 public:
+
+	// IClientSessionManager
+	bool Initialize(ILayer &bottomLayer, MessagesQueue &queue, SessionState initialState, IClientDAL &DAL, IClientManager &clientManager);
+	bool Start();
+	string GetClientName();
+
 	ClientSession();
 	~ClientSession();
-};
 
+private:
+	// Bottom layer for sending and receiving messages
+	ILayer* bottomLayer;
+	// Message queue for sending client's messages
+	MessagesQueue* messagesQueue;
+	//Current state of client session
+	SessionState sessionState;
+	// Data Access Layer
+	IClientDAL* DAL;
+	//Client Manager interface (used to register client ended)
+	IClientManager* clientManager;
+
+};
--- Pw.Elka.TIN/Server/Source Files/Components/ClientSession/ClientSession.cpp
+++ Pw.Elka.TIN/Server/Source Files/Components/ClientSession/ClientSession.cpp
@@ -1,5 +1,25 @@
 #include "../../../Header Files/Components/ClientSession/ClientSession.h"
 
+bool ClientSession::Initialize(ILayer &bottomLayer, MessagesQueue &queue, SessionState initialState, IClientDAL &DAL, IClientManager &clientManager)
+{
+	this->bottomLayer = &bottomLayer;
+	this->messagesQueue = &queue;
+	this->sessionState = initialState;
+	this->DAL = &DAL;
+	this->clientManager = &clientManager;
+
+	return true; 
+}
+
+bool ClientSession::Start()
+{
+	throw "Unimplemented";
+}
+string ClientSession::GetClientName()
+{
+	throw "Unimplemented";
+}
+
 
 
 ClientSession::ClientSession()
