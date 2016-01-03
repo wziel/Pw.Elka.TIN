f076c02dd2fc6dc848f40f077bb7004ca24dfff3
Asia R <asiaracz@wp.pl>
2016-01-03 16:58:28 +0100

    IClientSession - fix ServComERRADDR.cpp - untracked file

---
+++ Pw.Elka.TIN/Server/Source Files/Components/ClientSession/Communicates/ServComERRADDR.cpp
@@ -0,0 +1,15 @@
+#include "../../../../Header Files/Components/ClientSession/Communicates/ServComERRADDR.h"
+
+
+ServComERRADDR::ServComERRADDR()
+{
+	size = 1;
+	communicateBuffer = new char;
+	communicateBuffer[0] = _SERVCOMERRADDR;
+}
+
+
+ServComERRADDR::~ServComERRADDR()
+{
+	delete communicateBuffer;
+}
