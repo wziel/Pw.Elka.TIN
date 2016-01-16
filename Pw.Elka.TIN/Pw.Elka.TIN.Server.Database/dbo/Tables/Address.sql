b610d7e32b256271f35fba0ab9f44449553d0cc3
Michał Mudel <m.mudel@o2.pl>
2016-01-16 23:29:31 +0100

    Add unique constraint on pairs of data

--- Pw.Elka.TIN/Pw.Elka.TIN.Server.Database/dbo/Tables/Address.sql
+++ Pw.Elka.TIN/Pw.Elka.TIN.Server.Database/dbo/Tables/Address.sql
@@ -3,8 +3,12 @@
     [addressee_name] VARCHAR (50) NOT NULL,
     [address_name]   VARCHAR (50) NOT NULL,
     [id_client]      INT          NOT NULL,
-    CONSTRAINT [PK_Address_1] PRIMARY KEY CLUSTERED ([id_address] ASC)
+    CONSTRAINT [PK_Address_1] PRIMARY KEY CLUSTERED ([id_address] ASC),
+    CONSTRAINT [adresNazwa_klient] UNIQUE NONCLUSTERED ([address_name] ASC, [id_client] ASC),
+    CONSTRAINT [adresWartosc_klient] UNIQUE NONCLUSTERED ([addressee_name] ASC, [id_client] ASC)
 );
+
+
 
 
 
--- Pw.Elka.TIN/Pw.Elka.TIN.Server.Database/dbo/Tables/Group.sql
+++ Pw.Elka.TIN/Pw.Elka.TIN.Server.Database/dbo/Tables/Group.sql
@@ -3,6 +3,9 @@
     [id_client] INT           NOT NULL,
     [name]      VARCHAR (100) NOT NULL,
     CONSTRAINT [PK_Group] PRIMARY KEY CLUSTERED ([id_group] ASC),
-    CONSTRAINT [FK_Group_Client] FOREIGN KEY ([id_client]) REFERENCES [dbo].[Client] ([id_client])
+    CONSTRAINT [FK_Group_Client] FOREIGN KEY ([id_client]) REFERENCES [dbo].[Client] ([id_client]),
+    CONSTRAINT [grupa_klient] UNIQUE NONCLUSTERED ([id_client] ASC, [name] ASC)
 );
+
+
 
