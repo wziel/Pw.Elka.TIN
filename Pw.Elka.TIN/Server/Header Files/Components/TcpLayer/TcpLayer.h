bceefa34d9f669a2209eb7c188c5a5ff10f0b1bb
Asia R <asiaracz@wp.pl>
2016-01-21 14:31:19 +0100

    TcpLayer update - unfinished

--- Pw.Elka.TIN/Server/Header Files/Components/TcpLayer/TcpLayer.h
+++ Pw.Elka.TIN/Server/Header Files/Components/TcpLayer/TcpLayer.h
@@ -1,26 +1,13 @@
 #pragma once
 #include "../../Interfaces/ILayer.h"
-#include "../../Interfaces/ITcpLayerManager.h"
-
-#include <WinSock2.h>
-//#include <windows.h>
-//#include <sys/types.h>
-//#include <winsock.h>
-
 
 class TcpLayer :
-	public ILayer, public ITcpLayerManager
+	public ILayer
 {
 public:
-	TcpLayer(int socketfd);
+	TcpLayer(ILayer &bottomLayer);
 	~TcpLayer();
-//ITcpLayerManager intereface
-	///Stops TCPLayer from listening to events, so that ClientSession may end
-	///Returns 0 if successful
-	int End();
-	///Closes windows socket
-	///Returns 0 if successful
-	int CloseSocket();
+
 //ILayer interface
 	///Sends data to client
 	void Send(unsigned char* buffer, int size);
@@ -28,23 +15,13 @@ public:
 	void Receive(unsigned char* &buffer, int &size);
 
 private:
-///Communication socket descriptor
-	int socketFD;
-///Array of network events
-	WSAEVENT WSAEventArray[2];
-///Network events associated with WSAEventArray[0]
-	WSANETWORKEVENTS NetworkEvents;
-///Helper variable - result of function call
-	int iResult;
-///Signalled event
-	int signalledEvent; 
 ///Buffer for receiving and sending data (excluding header)
 	unsigned char* myBuffer;
 ///Size of buffer for data
 	int mySize; 
-///Size of buffer for data (byte form)
-	unsigned char mySizeBuffer[2];
 
+///Bottom layer
+	ILayer *bottomLayer;
 
 
 };
--- Pw.Elka.TIN/Server/Source Files/Components/TcpLayer/TcpLayer.cpp
+++ Pw.Elka.TIN/Server/Source Files/Components/TcpLayer/TcpLayer.cpp
@@ -2,43 +2,12 @@
 
 #include "../../../Header Files/Components/TcpLayer/TcpLayer.h"
 
-TcpLayer::TcpLayer(int socketfd)
-	: socketFD(socketfd)
+TcpLayer::TcpLayer(ILayer &bottomLayer)
 {
-	WSAEventArray[0] = WSACreateEvent();	//event signalling end of client session
-	if (WSAEventArray[0] == WSA_INVALID_EVENT)
-	{
-		throw "WSA error";
-	}
-
-	WSAEventArray[1] = WSACreateEvent();	//network event - read
-	if (WSAEventArray[1] == WSA_INVALID_EVENT)
-	{
-		throw "WSA error";
-	}
-	iResult = WSAEventSelect(socketFD, WSAEventArray[1], FD_READ | FD_CLOSE);	//associate event with a socket
-	if (iResult != 0)
-	{
-		throw "WSA error";
-	}
+	this->bottomLayer = &bottomLayer;
 }
 
-int TcpLayer::End()
-{
-	if (WSASetEvent(WSAEventArray[0]) == TRUE)	//successfully signal an ending event
-		return 0;
-	
-	else
-		throw "WSA error";
-}
 
-int TcpLayer::CloseSocket()
-{
-	if (closesocket(socketFD) == 0)
-		return 0;
-	else
-		throw "WSA error";
-}
 
 void TcpLayer::Send(unsigned char* buffer, int size)	//send data to client
 {
@@ -53,61 +22,22 @@ void TcpLayer::Send(unsigned char* buffer, int size)	//send data to client
 		myBuffer[i] = buffer[i - 2];
 	}
 
-	iResult = send(socketFD, (char*)myBuffer, mySize, NULL);	//send message
-	if (iResult == SOCKET_ERROR)
-	{
-		throw "Network error";
-	}
+	bottomLayer->Send(myBuffer, mySize);
 
 	delete[] myBuffer;
 }
 
 void TcpLayer::Receive(unsigned char* &buffer, int &size)	//receive data from client
 {
+	mySize = (((mySizeBuffer[1]) << 8) | (mySizeBuffer[0]));
+	size = mySize;
 
-	signalledEvent = WSAWaitForMultipleEvents(2, WSAEventArray, FALSE, WSA_INFINITE, FALSE);	//wait for events - receive a message or end TcpLayer
-	if (signalledEvent == WSA_WAIT_FAILED)
-	{
-		throw "WSA error";
-	}
+	buffer = new unsigned char[mySize];
 
-	if ((signalledEvent - WSA_WAIT_EVENT_0) == 0) // end of TcpLayer
-	{
-		throw "Client ended";
-	}
+	iResult = recv(socketFD, (char*)buffer, mySize, NULL);
 
-	else if ((signalledEvent - WSA_WAIT_EVENT_0) == 1) //receive a message or client distonnected
-	{
-		WSAEnumNetworkEvents(socketFD, WSAEventArray[1], &NetworkEvents);
-		if (NetworkEvents.lNetworkEvents & FD_READ)
-		{
-			iResult = recv(socketFD, (char*)mySizeBuffer, 2, NULL); //reading header (contains data size)
-			if (iResult == SOCKET_ERROR)
-			{
-				throw "Network error";
-			}
-
-			mySize = (((mySizeBuffer[1]) << 8) | (mySizeBuffer[0]));
-			size = mySize;
-
-			buffer = new unsigned char[mySize];
-
-			iResult = recv(socketFD, (char*)buffer, mySize, NULL);
-			if (iResult == SOCKET_ERROR)
-			{
-				throw "Network error";
-			}
-		}
-		else if (NetworkEvents.lNetworkEvents & FD_CLOSE)
-			throw "Client ended externally";
-		
-
-		WSAResetEvent(WSAEventArray[1]);
-	}
 }
 
 TcpLayer::~TcpLayer()
 {
-	WSACloseEvent(WSAEventArray[0]);
-	WSACloseEvent(WSAEventArray[1]);
 }
